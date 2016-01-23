f1cfae7c6fdf205a1b1ad3b206ff828146537086
yomotsu <oyamada@pxgrid.com>
2016-01-24 08:18:26 +0900

    fix typo

--- src/GLSlideshow.js
+++ src/GLSlideshow.js
@@ -1,14 +1,14 @@
 import utils              from './utils.js';
 import WebGLRenderer      from './WebGLRenderer.js';
 import CanvasRenderer     from './CanvasRenderer.js';
-import audoDetectRenderer from './audoDetectRenderer.js';
+import autoDetectRenderer from './autoDetectRenderer.js';
 import shaderLib          from './shaderLib.js';
 
 export default {
 
 	hasCanvas:          utils.hasCanvas,
 	hasWebGL:           utils.hasWebGL,
-	audoDetectRenderer: audoDetectRenderer,
+	autoDetectRenderer: autoDetectRenderer,
 	WebGLRenderer:      WebGLRenderer,
 	CanvasRenderer:     CanvasRenderer,
 	shaderLib:          shaderLib
